33eb94f793f2cd5fb05c87d0e310a41e4a9bf650
Patrick A <akolo@hotmail.com>
2019-04-20 21:18:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,5 +66,8 @@ class Owner
     "I am a #{species}."
   end
   
+  def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
   
 end
