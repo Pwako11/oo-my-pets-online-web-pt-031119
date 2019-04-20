698ef3279aef2ac18b7324023d27df4e797e0abc
Patrick A <akolo@hotmail.com>
2019-04-20 21:12:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,23 @@
 class Owner
   # code goes here
   
+  class Owner
+  OWNERS = []
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  def self.reset_all
+    OWNERS.clear
+  end
+
+  def self.all
+    OWNERS
+  end
+
+  def self.count
+    OWNERS.size
+  end
+  
+  
   
 end
